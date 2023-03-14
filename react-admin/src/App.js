9c6cf7f75f7637b0f7de28f8e2c2169caa8fc01f
Dibeisi <tomekszyluk@gmail.com>
2023-03-14 15:28:39 +0100

    dashboard

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -3,17 +3,17 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from "./scenes/global/Sidebar"
-import Dashboard from './scenes/dashboard';
-import Team from './scenes/team';
-import Contacts from './scenes/contacts';
-import Invoices from './scenes/invoices';
-import Form from './scenes/form';
-import Calendar from './scenes/calendar';
-import FAQ from './scenes/faq';
-import Bar from './scenes/bar';
-import Pie from './scenes/pie';
-import Line from './scenes/line';
-import Geography from './scenes/geography';
+import Dashboard from './scenes/dashboard/dashboard';
+import Team from './scenes/team/team';
+import Contacts from './scenes/contacts/contacts';
+import Invoices from './scenes/invoices/invoices';
+import Form from './scenes/form/form';
+import Calendar from './scenes/calendar/calendar';
+import FAQ from './scenes/faq/faq';
+import Bar from './scenes/bar/bar';
+import Pie from './scenes/pie/team';
+import Line from './scenes/line/line';
+import Geography from './scenes/geography/geography';
 
 function App() {
   const [theme, colorMode] = useMode();
